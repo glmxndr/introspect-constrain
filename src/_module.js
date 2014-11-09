31e206a06c7109e1b7e9cd7232112cc454895895
GhiOm <subtenante@gmail.com>
2014-11-09 18:20:13 +0100

    v0.1.1 - correct API to work well in ES6 modules

---
+++ src/_module.js
@@ -0,0 +1,11 @@
+import { report, Report } from './report';
+import { failure, Failure } from './failure';
+import { constraint, Constraint } from './constraint';
+import { constraints, Constraints } from './constraints';
+
+export {
+  report, Report,
+  failure, Failure,
+  constraint, Constraint,
+  constraints, Constraints
+};
